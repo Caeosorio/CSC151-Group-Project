4237d99675ba16e009f1c764d96727ae47eeb13a
Caeosorio <94332937+Caeosorio@users.noreply.github.com>
2021-11-14 21:57:54 -0500

    Update Room.java

--- RoomsAndMore/src/Room.java
+++ RoomsAndMore/src/Room.java
@@ -1,2 +1,21 @@
-public class Room {
+public class Room {              
+      
+   private int squareF;       
+       
+    public Room(int sqF) {      
+         squareF = sqF;
+}
+
+    public int getSquareFeet() {   
+      return squareF;
+}
+
+        public int getCapacity() {     
+                int capacity;
+                   capacity = squareF/9;
+                   return capacity;
+}
+          public String toString() {    
+                return " Square feet of room is: " + getSquareFeet() + " and the capacity of the room is: " + getCapacity();
+  }
 }
